9535dc703eb857b6a5f169755b6ae19fcadba09c
Beyza Sena Erkoc <beyzasenaerkoc@gmail.com>
2024-11-02 13:11:39 +0300

    Değişiklik açıklaması

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -7,6 +7,7 @@ import utilities.Driver;
 
 import static utilities.ReusableMethods.dotenv;
 
+
 public class HomePage {
     public HomePage() {
         PageFactory.initElements(Driver.getDriver(), this);
